569e2e9461e1d4426258f0ef7f280ecea8bb1b6f
Cramzzzberry <janroebantuan@gmail.com>
2023-10-05 01:21:42 +0800

    Can transpose all chords

--- src/assets/scripts/change-key.js
+++ src/assets/scripts/change-key.js
@@ -16,25 +16,46 @@ export default function changeKey(str, newKey, prevKey) {
 
 function replacingKeys(str, keyDiff, newKey, prevKey) {
   let changedStr = ''
-  const keyObj = {
-    C: tranposeKey('C', keyDiff, newKey, prevKey),
-    D: tranposeKey('D', keyDiff, newKey, prevKey),
-    E: tranposeKey('E', keyDiff, newKey, prevKey),
-    F: tranposeKey('F', keyDiff, newKey, prevKey),
-    G: tranposeKey('G', keyDiff, newKey, prevKey),
-    A: tranposeKey('A', keyDiff, newKey, prevKey),
-    B: tranposeKey('B', keyDiff, newKey, prevKey),
+  const keysObj = {
+    'C': tranposeKey('C', keyDiff, newKey, prevKey),
+    'C#': tranposeKey('C#', keyDiff, newKey, prevKey),
+    'Db': tranposeKey('Db', keyDiff, newKey, prevKey),
+    'D': tranposeKey('D', keyDiff, newKey, prevKey),
+    'D#': tranposeKey('D#', keyDiff, newKey, prevKey),
+    'Eb': tranposeKey('Eb', keyDiff, newKey, prevKey),
+    'E': tranposeKey('E', keyDiff, newKey, prevKey),
+    'F': tranposeKey('F', keyDiff, newKey, prevKey),
+    'F#': tranposeKey('F#', keyDiff, newKey, prevKey),
+    'Gb': tranposeKey('Gb', keyDiff, newKey, prevKey),
+    'G': tranposeKey('G', keyDiff, newKey, prevKey),
+    'G#': tranposeKey('G#', keyDiff, newKey, prevKey),
+    'Ab': tranposeKey('Ab', keyDiff, newKey, prevKey),
+    'A': tranposeKey('A', keyDiff, newKey, prevKey),
+    'A#': tranposeKey('A#', keyDiff, newKey, prevKey),
+    'Bb': tranposeKey('Bb', keyDiff, newKey, prevKey),
+    'B': tranposeKey('B', keyDiff, newKey, prevKey),
   }
 
   changedStr = str.replace(new RegExp(
     checkKey('C') + "|" +
+    checkKey('C#') + "|" +
+    checkKey('Db') + "|" +
     checkKey('D') + "|" +
+    checkKey('D#') + "|" +
+    checkKey('Eb') + "|" +
     checkKey('E') + "|" +
     checkKey('F') + "|" +
+    checkKey('F#') + "|" +
+    checkKey('Gb') + "|" +
     checkKey('G') + "|" +
+    checkKey('G#') + "|" +
+    checkKey('Ab') + "|" +
     checkKey('A') + "|" +
+    checkKey('A#') + "|" +
+    checkKey('Bb') + "|" +
     checkKey('B')
-    , 'g'), function(match) { return keyObj[match] })
+    , 'g'), function(match) { return keysObj[match] }
+  )
 
   return changedStr
 }
@@ -45,6 +66,18 @@ function tranposeKey(key, keyDiff, newKey, prevKey) {
   let newKeyIndex = keys.indexOf(newKey) + 1
   let prevKeyIndex = keys.indexOf(prevKey) + 1
   let goHigher = newKeyIndex > prevKeyIndex
+
+  if (key === 'Db') {
+    key = 'C#'
+  } else if (key === 'D#') {
+    key = 'Eb'
+  } else if (key === 'Gb') {
+    key = 'F#'
+  } else if (key === 'Ab') {
+    key = 'G#'
+  } else if (key === 'A#') {
+    key = 'Bb'
+  }
 
   if (goHigher) {
     if ((keys.indexOf(key) + 1) + keyDiff > keys.length) {
@@ -60,19 +93,13 @@ function tranposeKey(key, keyDiff, newKey, prevKey) {
     }
   }
 
-  console.log(keys[changedKeyIndex])
+  // console.log(key + " -> " + keys[changedKeyIndex])
 
   return keys[changedKeyIndex]
 }
 
 function checkKey(chord) {
-  let regex = null
+  let regexStr = `(?<=^| |\n)${ chord }(?!#|b)(?=(maj|m|dim|sus|aug|add)?(Md)?d?(?!w))`
 
-  if(chord.includes('#')) {
-    regex = ``
-  } else {
-    regex = `(?<=^| |\n)${ chord }(?!#|b)(?=(maj|m|dim|sus|aug|add)?(Md)?d?(?!w))`
-  }
-
-  return regex
+  return regexStr
 }
