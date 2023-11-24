463895fbc2b193da26a4794dd91d2d7404344e7d
Cramzzzberry <janroebantuan@gmail.com>
2023-11-24 19:24:30 +0800

    Build #13 - changed the baseUrl on vite config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Qotes/',
+  base: '/qotes/',
   plugins: [vue()],
   resolve: {
     alias: {
