c790ddb8974238a7c67f9ac64ac77d70075bd2ed
Cramzzzberry <janroebantuan@gmail.com>
2023-11-28 11:34:59 +0800

    added icons, vite pwa and pwa generator

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,26 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/qotes/',
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      manifest: {
+        icons: [
+          {
+            src: '/icons/icon.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
